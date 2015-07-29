a8fe0aff6aeaf911f25678f307d5980068efcf1a
sofia <sofiam@barracuda-digital.co.uk>
2015-07-29 12:48:18 +0100

    customizing angulajs skills

--- angularjs/controllers/main.js
+++ angularjs/controllers/main.js
@@ -2,11 +2,22 @@
 var app = angular.module("myApp",[]);
 
 app.controller("mainController", function ($scope, $http) {
-
-    $http.get("json/skills.json")
+    $http.get("json/cms.json")
             .success(function (response) {
-                $scope.skills = response;
+                $scope.cms = response;
             });   
+    $http.get("json/languages.json")
+            .success(function (response) {
+                $scope.languages = response;
+            });   
+    $http.get("json/projects.json")
+            .success(function (response) {
+                $scope.projects = response;
+            }); 
+    $http.get("json/platforms.json")
+            .success(function (response) {
+                $scope.platforms = response;
+            });                   
 });
 
 
